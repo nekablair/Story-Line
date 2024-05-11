2a3d80ab5f9d96bbadcf0e678422821bf4eef0bd
Neka Blair <ourblairfamily@gmail.com>
2024-05-11 06:45:44 -0500

    scaffold backend

---
+++ backend/reading_game_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReadingGameAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reading_game_app'
---
+++ backend/user_app/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# thinking of adding parents as a group with greater control over the children's accounts
+# Create your models here.
